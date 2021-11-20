5ddd5b8416ae86d9140450b5319164e6c182eb0a
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-11-20 14:10:51 -0300

    Requisito 7 Done lint erros

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,6 @@ import Marquee from 'react-fast-marquee';
 const linkTrybe = 'https://assets-global.website-files.com/61549abf6fb9ca5e91bc5709/61549abf6fb9ca4630bc5747_Logo.svg';
 
 class Header extends React.Component {
-  // eslint-disable-next-line max-lines-per-function
   render() {
     const { email, expenses, currencies } = this.props;
     const totalExpenses = parseFloat(expenses.reduce((acc, curr) => {
@@ -18,29 +17,22 @@ class Header extends React.Component {
     return (
       <header className="navbar navbar-light bg-light">
         <div className="navbar-brand d-flex flex-row" href="#">
-          <img
-            src={ linkTrybe }
-            className="d-inline-block align-top mx-3"
-            alt=""
-          />
+          <img src={ linkTrybe } className="d-inline-block align-top mx-3" alt="" />
           <h2>Wallet</h2>
         </div>
         <div className="navbar-brand d-flex flex-row w-50" href="#">
-
           <Marquee>
             {
               Object.entries(currencies).map((currency) => (
-                <h6 key={currency[1].code } className="text-success">
+                <h6 key={ currency[1].code } className="text-success">
                   {currency[1].code}
                   &nbsp;-&nbsp;$
                   {currency[1].ask}
                   &nbsp;,&nbsp;&nbsp;
                 </h6>
-
               ))
             }
           </Marquee>
-
         </div>
         <form className="form-inline d-flex flex-row mx-3">
           <div className=" mr-sm-2" data-testid="email-field">
@@ -50,7 +42,6 @@ class Header extends React.Component {
           <div className="  d-flex mx-3">
             Despesa Total: &nbsp;
             <h6 data-testid="total-field">
-              {' '}
               R$
               { totalExpenses }
             </h6>
